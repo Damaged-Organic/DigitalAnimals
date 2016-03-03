edf498b2597039e8d7e1cca86b288562383951b8
Kid Binary <kid.binary@gmail.com>
2016-03-03 20:20:45 +0200

    Stage 1

--- digital_animals/website/models.py
+++ digital_animals/website/models.py
@@ -1,27 +1,31 @@
 from django.db import models
 
+
 class Benefit(models.Model):
     title = models.CharField(max_length=100)
-    text  = models.CharField(max_length=500)
-    icon  = models.CharField(max_length=50)
+    text = models.CharField(max_length=500)
+    icon = models.CharField(max_length=50)
 
     def __str__(self):
         return self.title
 
+
 class Feature(models.Model):
     title = models.CharField(max_length=100)
-    text  = models.CharField(max_length=200)
+    text = models.CharField(max_length=200)
     image = models.CharField(max_length=50)
 
     def __str__(self):
         return self.title
 
+
 class Step(models.Model):
     title = models.CharField(max_length=100)
-    icon  = models.CharField(max_length=50)
+    icon = models.CharField(max_length=50)
 
     def __str__(self):
         return self.title
+
 
 class Pricing(models.Model):
     category = models.CharField(max_length=100)
@@ -31,27 +35,13 @@ class Pricing(models.Model):
         return self.category
 
     @property
-    def price(self):
+    def price_signed(self):
         return "%s ₴" % self.price
+
 
 class Contact(models.Model):
     email = models.EmailField(max_length=254)
-    phone = models.IntegerField(max_length=12)
+    phone = models.CharField(max_length=19)
 
-    # Move this to twig filter
-    @property
-    def phone(self):
-
-        international = self.phone[0:3]
-        local = self.phone[3:5]
-        number = (self.phone[5:8], self.phone[8:10], self.phone[10:12])
-
-        pattern = "+{international} ({local}) {number_I}-{number_II}-{number_III}"
-
-        return pattern.format(
-            international = international,
-            local = local,
-            number_I = number.0,
-            number_II = number.1,
-            number_III = number.2
-        )
+    def __str__(self):
+        return self.email
