86441e21c66bde171145ef8a75fcb45d63f65cd9
Kid Binary <kid.binary@gmail.com>
2017-04-07 14:04:19 +0300

    uWSGI interface, base config fix

--- digital_animals/manage.py
+++ digital_animals/manage.py
@@ -5,7 +5,7 @@ import sys
 if __name__ == "__main__":
     os.environ.setdefault(
         "DJANGO_SETTINGS_MODULE",
-        "digital_animals.settings.production"
+        "digital_animals.settings.development"
     )
 
     from django.core.management import execute_from_command_line
